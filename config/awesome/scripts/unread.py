4225404781866a83c30f9b890479f5870ed296b1
Aurélien LAJOIE <orel@melix.net>
2014-11-19 17:19:17 +1100

    Manage several email account

--- config/awesome/scripts/unread.py
+++ config/awesome/scripts/unread.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python
 
 import imaplib
+import string
 import os
 
 #first field is imap server, second - port (993 for gmail SSL IMAP)
-M=imaplib.IMAP4_SSL("imap.intersec.com", 993)
+M=imaplib.IMAP4_SSL("ssl0.ovh.net", 993)
 #first field is imap login (gmail uses login with domain and '@' character), second - password
-M.login("XXXXXXXXXXXXXX","###############")
+M.login("XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXX")
 
-status, counts = M.status("Inbox","(MESSAGES UNSEEN)")
+status, counts = M.status("Inbox", "(MESSAGES UNSEEN)")
 
-unread = counts[0].split()[4][:-1]
-if unread == "0":
+M.logout()
+unread = string.atoi(counts[0].split()[4][:-1]);
+
+M=imaplib.IMAP4_SSL("ssl0.ovh.net", 993)
+M.login("XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXX")
+
+status, counts = M.status("Inbox", "(MESSAGES UNSEEN)")
+M.logout()
+
+unread += string.atoi(counts[0].split()[4][:-1]);
+
+if unread == 0:
     print " 0 "
     os.system("xset -led 3")
 else:
 #red fg color when you have unseen mail
-    print "<span color=\"red\">  <b>"+unread+"</b>  </span>"
+    print "<span color=\"red\">  <b>" + str(unread) + "</b>  </span>"
     os.system("xset led 3")
 
-M.logout()
