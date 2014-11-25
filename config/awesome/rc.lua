a96144765d77f1634bc7c3ec11d94beacc6ad035
Aurélien LAJOIE <aurelien.lajoie@utix.fr>
2014-11-25 16:15:29 +1100

    Awesome: display errors

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -5,7 +5,7 @@ require("awful.rules")
 -- Theme handling library
 require("beautiful")
 -- Notification library
-require("naughty")
+naughty = require("naughty")
 
 -- Load Debian menu entries
 require("debian.menu")
@@ -47,7 +47,30 @@ layouts =
 --    awful.layout.suit.floating
 }
 -- }}}
+-- {{{ Error handling
+-- Check if awesome encountered an error during startup and fell back to
+-- another config (This code will only ever execute for the fallback config)
+if awesome.startup_errors then
+    naughty.notify({ preset = naughty.config.presets.critical,
+                     title = "Oops, there were errors during startup!",
+                     text = awesome.startup_errors })
+end
 
+-- Handle runtime errors after startup
+do
+    local in_error = false
+    awesome.add_signal("debug::error", function (err)
+        -- Make sure we don't go into an endless error loop
+        if in_error then return end
+        in_error = true
+
+        naughty.notify({ preset = naughty.config.presets.critical,
+                         title = "Oops, an error happened!",
+                         text = err })
+        in_error = false
+    end)
+end
+-- }}}
 -- {{{ Tags
 -- Define a tag table which hold all screen tags.
 tags = {}
